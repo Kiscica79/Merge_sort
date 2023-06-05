ea0135dff4b597c8ba9175d183c3b04c4d517b46
Kiscica79 <oroszildiko79@gmail.hu>
2023-06-05 20:00:43 +0200

    add isSorted Method

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,34 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Merge sort");
 
     }
+
     public static boolean isSorted(int[] array) {
-        for (int i = 0; i < array.length-1 ; i++) {
-            if (array[i] > array[i+1]) {
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
                 return false;
             }
         }
         return true;
     }
 
-    public static int[] merge(int[] left, int[] right){
+    public static int[] merge(int[] left, int[] right) {
         return null;
     }
 
     public static int[] mergeSort(int[] array) {
-        return null;
+        if (isSorted(array)) {
+            return array;
+        }
+        int[] left = Arrays.copyOfRange(array, 0, array.length/2);
+        int[] right = Arrays.copyOfRange(array, array.length/2, array.length);
+        left = mergeSort(left);
+        right = mergeSort(right);
+        int[] merged = merge(left, right);
+        return merged;
     }
 
 }
